6fa2f7b48585f469749def54860800919fc2d1ef
HuiYa <k_hawon@naver.com>
2020-12-12 17:08:08 +0900

    git init

---
+++ src/main/java/io/storyclip/web/StoryClipApplication.java
@@ -0,0 +1,13 @@
+package io.storyclip.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryClipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoryClipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/storyclip/web/StoryClipApplicationTests.java
@@ -0,0 +1,13 @@
+package io.storyclip.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryClipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
