7d2874f0d3c0e697a92398cf725637c1c083be96
HuiYa <k_hawon@naver.com>
2020-12-18 23:57:42 +0900

    Result 객체 기본값 설정 및 User 필명 컬럼 추가

--- src/main/java/io/storyclip/web/Entity/Result.java
+++ src/main/java/io/storyclip/web/Entity/Result.java
@@ -1,5 +1,6 @@
 package io.storyclip.web.Entity;
 
+import io.storyclip.web.Type.Type;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,9 +9,9 @@ import lombok.ToString;
 @Setter
 @ToString
 public class Result {
-    private Boolean success;
-    private Enum message;
-    private Object resultData;
+    private Boolean success = false;
+    private Enum message = Type.RESULT_NOT_SET;
+    private Object resultData = null;
 
     public void setResult(Object resultData) {
         this.resultData = resultData;
--- src/main/java/io/storyclip/web/Type/Type.java
+++ src/main/java/io/storyclip/web/Type/Type.java
@@ -1,6 +1,18 @@
 package io.storyclip.web.Type;
 
 public enum Type {
+    /**
+     * 성공
+     */
     OK,
+
+    /**
+     * 에러
+     */
     ERROR,
+    
+    /**
+     * 결과가 등록되지 않음
+     */
+    RESULT_NOT_SET,
 }
